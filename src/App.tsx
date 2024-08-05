70c1466f72eda80b6fa39b4cfc195b80ad8c6bd0
Petr <peachtrees2009@gmail.com>
2024-08-05 23:17:34 +0300

    05082024-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+// import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className='test'>Hello</h1>
+        <button className="uk-button uk-button-default">Default</button>
+      <button className="uk-button uk-button-primary">Primary</button>
+      <button className="uk-button uk-button-secondary">Secondary</button>
+      <button className="uk-button uk-button-danger">Danger</button>
+      <button className="uk-button uk-button-text">Text</button>
+      <button className="uk-button uk-button-link">Link</button>
     </div>
   );
 }
